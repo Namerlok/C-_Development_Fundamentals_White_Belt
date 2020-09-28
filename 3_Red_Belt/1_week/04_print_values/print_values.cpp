f33d7dd20ea09b4324317015a88b8929b90c367c
Namerlok <Namerlok@yandex.ru>
2020-09-28 19:40:41 +0300

    adding the first part of the first week

---
+++ 3_Red_Belt/1_week/04_print_values/print_values.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : print_values.cpp
+// Author      : Namerlok
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Ansi-style
+//============================================================================
+
+#include "test_runner.h"
+
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+#define PRINT_VALUES(out, x, y) (out) << (x) << endl << (y) << endl
+
+
+int main() {
+	TestRunner tr;
+	tr.RunTest([] {
+		ostringstream output;
+		PRINT_VALUES(output, 5, "red belt");
+		ASSERT_EQUAL(output.str(), "5\nred belt\n");
+	}, "PRINT_VALUES usage example");
+}
+
+
